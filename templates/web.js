821601b44b9644fc4c22c9e3dcaf6d699e28c574
Daniel-Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2025-08-04 09:13:06 +0000

    fix: remove comments from web.js

--- templates/web.js
+++ templates/web.js
@@ -20,18 +20,7 @@ class LGallery {
       This license key was graciously provided by LightGallery under their
       GPLv3 open-source project license:
       */
-      //licenseKey: '8FFA6495-676C4D30-8BFC54B6-4D0A6CEC'
       licenseKey: '0000-0000-000-0000'
-      
-      /*
-      Please do not take it and use it for other projects, as it was provided
-      specifically for Immich Public Proxy.
-
-      For your own projects you can use the default license key of
-      0000-0000-000-0000 as per their docs:
-
-      https://www.lightgalleryjs.com/docs/settings/#licenseKey
-      */
     }, params.lgConfig));
     this.items = params.items;
 
